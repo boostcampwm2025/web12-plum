5b3b682a18ca1e2366c8056169ba27aa7d8f2b8d
Jaehyun Yoon <yuchem2@gmail.com>
2026-01-05 21:12:37 +0900

    chore: 프론트엔드 모니터링 도구 추가 (#15)
    - 개발 환경에서 vercel analytics + sentry 모니터링 추가
    - 운영 환경에서 vercel analytics + vercel speed insights + sentry 모니터링 추가
    - 빌드 시 sentry에 source map 전송

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,13 +1,30 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    }
-  }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      sentryVitePlugin({
+        org: env.ORG_NAME,
+        project: env.PROJECT_NAME,
+        authToken: env.SENTRY_AUTH_TOKEN,
+      }),
+    ],
+    build: {
+      sourcemap: true,
+    },
+    base: '/',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  };
 });
