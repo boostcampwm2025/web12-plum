bbfe759141affc22977842a039fe05088f93e31b
DongGyun <wooheuy5752@gmail.com>
2026-01-06 12:43:50 +0900

    chore: 새 아이콘 등록

--- apps/frontend/src/shared/components/icon/iconMap.ts
+++ apps/frontend/src/shared/components/icon/iconMap.ts
@@ -1,35 +1,81 @@
+import iconsBreakdownRoom from '@/assets/icons/breakdown-room.svg?react';
+import iconsCamDisabled from '@/assets/icons/cam-disabled.svg?react';
+import iconsCam from '@/assets/icons/cam.svg?react';
+import iconsChat from '@/assets/icons/chat.svg?react';
 import iconsCheckBoxFilled from '@/assets/icons/check-box-filled.svg?react';
 import iconsCheckBox from '@/assets/icons/check-box.svg?react';
-import iconsChevronRight from '@/assets/icons/chevron-right.svg?react';
-import iconsClose from '@/assets/icons/close.svg?react';
+import iconsCheck from '@/assets/icons/check.svg?react';
+import iconsChevron from '@/assets/icons/chevron.svg?react';
 import iconsDownload from '@/assets/icons/download.svg?react';
-import iconsHand from '@/assets/icons/hand.svg?react';
-import iconsMaximizing from '@/assets/icons/maximizing.svg?react';
-import iconsMedal from '@/assets/icons/medal.svg?react';
+import iconsExit from '@/assets/icons/exit.svg?react';
+import iconsInfo from '@/assets/icons/info.svg?react';
+import iconsInteraction from '@/assets/icons/interaction.svg?react';
+import iconsMaximize from '@/assets/icons/maximize.svg?react';
+import iconsMegaphone from '@/assets/icons/megaphone.svg?react';
+import iconsMenu from '@/assets/icons/menu.svg?react';
+import iconsMic from '@/assets/icons/mic.svg?react';
+import iconsMic_disabled from '@/assets/icons/mic_disabled.svg?react';
+import iconsMinimize from '@/assets/icons/minimize.svg?react';
 import iconsMinus from '@/assets/icons/minus.svg?react';
+import iconsPencil from '@/assets/icons/pencil.svg?react';
+import iconsPip from '@/assets/icons/pip.svg?react';
 import iconsPlus from '@/assets/icons/plus.svg?react';
+import iconsQna from '@/assets/icons/qna.svg?react';
+import iconsRanking from '@/assets/icons/ranking.svg?react';
+import iconsScreen_share from '@/assets/icons/screen_share.svg?react';
+import iconsSend from '@/assets/icons/send.svg?react';
+import iconsSideOpen from '@/assets/icons/side-open.svg?react';
+import iconsSiren from '@/assets/icons/siren.svg?react';
+import iconsStart from '@/assets/icons/start.svg?react';
+import iconsStop from '@/assets/icons/stop.svg?react';
+import iconsTimer from '@/assets/icons/timer.svg?react';
 import iconsTrash from '@/assets/icons/trash.svg?react';
+import iconsTrend from '@/assets/icons/trend.svg?react';
 import iconsUpload from '@/assets/icons/upload.svg?react';
-import iconsVideoOff from '@/assets/icons/video-off.svg?react';
-import iconsWarning from '@/assets/icons/warning.svg?react';
+import iconsUsers from '@/assets/icons/users.svg?react';
+import iconsVote from '@/assets/icons/vote.svg?react';
+import iconsX from '@/assets/icons/x.svg?react';
 
 import type { ComponentType, SVGProps } from 'react';
 
 export const iconMap = {
+  'breakdown-room': iconsBreakdownRoom,
+  'cam-disabled': iconsCamDisabled,
+  cam: iconsCam,
+  chat: iconsChat,
   'check-box-filled': iconsCheckBoxFilled,
   'check-box': iconsCheckBox,
-  'chevron-right': iconsChevronRight,
-  close: iconsClose,
+  check: iconsCheck,
+  chevron: iconsChevron,
   download: iconsDownload,
-  hand: iconsHand,
-  maximizing: iconsMaximizing,
-  medal: iconsMedal,
+  exit: iconsExit,
+  info: iconsInfo,
+  interaction: iconsInteraction,
+  maximize: iconsMaximize,
+  megaphone: iconsMegaphone,
+  menu: iconsMenu,
+  mic: iconsMic,
+  mic_disabled: iconsMic_disabled,
+  minimize: iconsMinimize,
   minus: iconsMinus,
+  pencil: iconsPencil,
+  pip: iconsPip,
   plus: iconsPlus,
+  qna: iconsQna,
+  ranking: iconsRanking,
+  screen_share: iconsScreen_share,
+  send: iconsSend,
+  'side-open': iconsSideOpen,
+  siren: iconsSiren,
+  start: iconsStart,
+  stop: iconsStop,
+  timer: iconsTimer,
   trash: iconsTrash,
+  trend: iconsTrend,
   upload: iconsUpload,
-  'video-off': iconsVideoOff,
-  warning: iconsWarning,
+  users: iconsUsers,
+  vote: iconsVote,
+  x: iconsX,
 };
 
 export type IconName = keyof typeof iconMap;
