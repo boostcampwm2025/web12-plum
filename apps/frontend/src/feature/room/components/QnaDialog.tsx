feb5291a72bcaa1368a3ed39f88f99eeb92e0594
Yuncheol Kwak <lvbhkwak@gmail.com>
2026-01-28 23:36:50 +0900

    refactor: early return 적용

--- apps/frontend/src/feature/room/components/QnaDialog.tsx
+++ apps/frontend/src/feature/room/components/QnaDialog.tsx
@@ -14,44 +14,44 @@ export function QnaDialog({ qna, startedAt, onSubmit, isSubmitted = false }: Qna
   const [text, setText] = useState('');
   const isDisabled = !qna || isSubmitted || text.trim().length === 0;
 
+  if (!qna) {
+    return (
+      <div className="text-subtext mb-2 flex justify-center">현재 진행중인 Q&A가 없습니다</div>
+    );
+  }
+
   return (
-    <>
-      {qna ? (
-        <div className="flex flex-col gap-4">
-          <h3 className="text-text w-full text-2xl font-bold">{qna.title}</h3>
+    <div className="flex flex-col gap-4">
+      <h3 className="text-text w-full text-2xl font-bold">{qna.title}</h3>
 
-          {isSubmitted ? (
-            <div className="text-subtext-light flex min-h-24 w-full items-center justify-center rounded-lg bg-gray-300 text-sm">
-              답변 제출이 완료되었습니다.
-            </div>
-          ) : (
-            <textarea
-              className="text-text placeholder:text-subtext-light focus:ring-primary text-md min-h-24 w-full resize-none rounded-lg bg-gray-300 p-3 outline-none"
-              placeholder="답변을 입력해 주세요"
-              value={text}
-              onChange={(event) => setText(event.target.value)}
-            />
-          )}
-
-          <Button
-            className="text-sm"
-            onClick={() => {
-              if (!qna || isDisabled) return;
-              onSubmit?.(qna.id, text.trim());
-              setText('');
-            }}
-            disabled={isDisabled}
-          >
-            답변 보내기
-          </Button>
-          <TimeLeft
-            timeLimitSeconds={qna.timeLimit}
-            startedAt={startedAt}
-          />
+      {isSubmitted ? (
+        <div className="text-subtext-light flex min-h-24 w-full items-center justify-center rounded-lg bg-gray-300 text-sm">
+          답변 제출이 완료되었습니다.
         </div>
       ) : (
-        <div className="text-subtext mb-2 flex justify-center">현재 진행중인 Q&A가 없습니다</div>
+        <textarea
+          className="text-text placeholder:text-subtext-light focus:ring-primary text-md min-h-24 w-full resize-none rounded-lg bg-gray-300 p-3 outline-none"
+          placeholder="답변을 입력해 주세요"
+          value={text}
+          onChange={(event) => setText(event.target.value)}
+        />
       )}
-    </>
+
+      <Button
+        className="text-sm"
+        onClick={() => {
+          if (!qna || isDisabled) return;
+          onSubmit?.(qna.id, text.trim());
+          setText('');
+        }}
+        disabled={isDisabled}
+      >
+        답변 보내기
+      </Button>
+      <TimeLeft
+        timeLimitSeconds={qna.timeLimit}
+        startedAt={startedAt}
+      />
+    </div>
   );
 }
