7b68b220ba077bda29c32feef0153fb591bd2fa8
Jaehyun Yoon <yuchem2@gmail.com>
2026-02-04 23:41:49 +0900

    fix: Gaurd가 WsException을 사용하게끔 수정

--- apps/backend/src/common/guard/ws-action.guard.ts
+++ apps/backend/src/common/guard/ws-action.guard.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { WsException } from '@nestjs/websockets';
 import { Participant, Room } from '@plum/shared-interfaces';
 
-import { BusinessException } from '../types/index.js';
 import { WsBaseGuard } from './ws-base.guard.js';
 
 @Injectable()
 export class WsPresenterGuard extends WsBaseGuard {
   protected validateRole(room: Room, participant: Participant) {
-    if (participant.role !== 'presenter' || room.presenter !== participant.id) {
-      throw new BusinessException('발표자 권한이 없습니다.');
-    }
     if (room.status !== 'active') {
-      throw new BusinessException('활성화된 강의가 아닙니다.');
+      throw new WsException('활성화된 강의가 아닙니다.');
+    }
+    if (participant.role !== 'presenter' || room.presenter !== participant.id) {
+      throw new WsException('발표자 권한이 없습니다.');
     }
   }
 }
@@ -19,11 +19,11 @@ export class WsPresenterGuard extends WsBaseGuard {
 @Injectable()
 export class WsAudienceGuard extends WsBaseGuard {
   protected validateRole(room: Room, participant: Participant) {
-    if (participant.role !== 'audience') {
-      throw new BusinessException('청중 권한이 없습니다.');
-    }
     if (room.status !== 'active') {
-      throw new BusinessException('활성화된 강의가 아닙니다.');
+      throw new WsException('활성화된 강의가 아닙니다.');
+    }
+    if (participant.role !== 'audience') {
+      throw new WsException('청중 권한이 없습니다.');
     }
   }
 }
@@ -32,7 +32,7 @@ export class WsAudienceGuard extends WsBaseGuard {
 export class WsAuthGuard extends WsBaseGuard {
   protected validateRole(room: Room, _participant: Participant) {
     if (room.status !== 'active') {
-      throw new BusinessException('활성화된 강의가 아닙니다.');
+      throw new WsException('활성화된 강의가 아닙니다.');
     }
   }
 }
